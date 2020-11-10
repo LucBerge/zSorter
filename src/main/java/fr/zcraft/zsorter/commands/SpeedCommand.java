649004e428594a9fc9ef3bb37fded0e7bd621291
Luc Berge <lucas.bergeron@outlook.fr>
2020-11-10 01:02:49 +0100

    Replaced "sorter speed" by "sorting speed"

--- src/main/java/fr/zcraft/zsorter/commands/SpeedCommand.java
+++ src/main/java/fr/zcraft/zsorter/commands/SpeedCommand.java
@@ -19,7 +19,7 @@ public class SpeedCommand extends ZSorterCommands{
     	
     	//Check the number of arguments
         if (args.length < 2)
-            throwInvalidArgument(I.t("A sorter name and a sorter speed are required."));
+            throwInvalidArgument(I.t("A sorter name and a sorting speed are required."));
 
         //Get the name
         String name = args[0];
@@ -29,16 +29,16 @@ public class SpeedCommand extends ZSorterCommands{
         try {
         	speed = Integer.parseInt(args[1]);
             if (speed < 1 || 64 < speed)
-                throwInvalidArgument(I.t("The sorter speed must be between 1 and 64."));
+                throwInvalidArgument(I.t("The sorting speed must be between 1 and 64."));
         }catch(NumberFormatException e) {
-            throwInvalidArgument(I.t("The sorter speed must be an integer."));
+            throwInvalidArgument(I.t("The sorting speed must be an integer."));
         }
 
         //Get the sorter
         Sorter sorter = ZSorter.getInstance().getSorterManager().getNameToSorter().get(name);
         if(sorter != null) {
         	sorter.setSpeed(speed);
-            success(I.t("The sorter speed of the sorter has been set to {0}.", speed));
+            success(I.t("The sorting speed of the sorter has been set to {0}.", speed));
         }
         else {
             error(I.t("There is no sorter with this name."));
